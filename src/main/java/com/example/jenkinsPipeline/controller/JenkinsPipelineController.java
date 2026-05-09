534f4bc9dc747631cfe4f0628a05a3aaec6ba05e
Jeevitha <jeevithakumari.mani123@gmail.com>
2026-05-09 10:41:53 +0530

    Docker file name changes

---
+++ src/main/java/com/example/jenkinsPipeline/controller/JenkinsPipelineController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.DemoApplication;
+
+@RestController
+@RequestMapping("/demoJen")
+public class JenkinsPipelineController {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsPipelineController.class);
+
+	@GetMapping("/getCtrl")
+	public String getDemoController() {
+		logger.info("Inside JenkinsPipelineController");
+		return "This is a Jenkins controller";
+	}
+}
